dec4ff967d6c27350e232a87adc4fe8e145bb75f
Brendan K <brendanmkelly94@gmail.com>
2019-03-29 17:22:22 -0500

    fixed redirect so that when a join request is accepted, the user can access '/home'

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,20 +4,20 @@ var path = require("path");
 
 var isAuthenticated = require("../config/middleware/isAuthenticated");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Load index page
-  app.get("/", function(req, res) {
+  app.get("/", function (req, res) {
     res.render("index");
   });
 
   //user Auth routes
-  app.get("/signup", function(req, res){
+  app.get("/signup", function (req, res) {
     res.render("signup");
   })
-  
 
-  app.get("/login", function(req, res){
-    if(req.user){
+
+  app.get("/login", function (req, res) {
+    if (req.user) {
       res.redirect("/home");
     }
     res.render("login");
@@ -25,111 +25,120 @@ module.exports = function(app) {
 
   //Load home page for authenticated user
 
-  app.get("/home",isAuthenticated, function(req, res){
+  app.get("/home", isAuthenticated, function (req, res) {
     console.log("redirected")
     console.log(req.user);
     var homeObject = {
       user: "",
       group: "",
+      groupMembers: "",
       bill: "",
-      chore: "",
-      grocery: ""
+      grocery: "",
+      chore: ""
     }
-    //If the user has a group send them to the group home page
-    if (req.user.GroupId){
-      //Find the user row for the logged in user
-      db.User.findOne({
-        where: {
-          id: req.user.id
-        }
-      }).then(function(userData){
-          homeObject.user = userData
-        })
-      //find the group row associated with the user
-      db.Group.findOne({
-        where: {
-          id: req.user.GroupId
-        }
-      }).then(function(groupData){
-          homeObject.group = groupData   
-        })
-      //find all bill rows associated with the group
-      db.Bill.findAll({
-        where: {
-          GroupId: req.user.GroupId
-        }
-      }).then(function(billData){
-        homeObject.bill = billData
+    //Find the user row for the logged in user & then assign the value of the User's GroupId from the database to the req.user object
+    db.User.findOne({
+      where: {
+        id: req.user.id
+      }
+    }).then(function (userData) {
+      homeObject.user = userData;
+      console.log(userData.GroupId)
+      req.user.GroupId = userData.GroupId;
+      //If the user has a group send them to the group home page
+      if (req.user.GroupId) {
+        //find all the group members in the current user's group and return their names and ids
+        db.User.findAll({
+          where:{
+            GroupId: req.user.GroupId
+          },
+          attributes: ["name", "id"]
+        }).then(function (membersData){
+          homeObject.groupMembers = membersData;
+        });
+        //find the group row associated with the user
+        db.Group.findOne({
+          where: {
+            id: req.user.GroupId
+          }
+        }).then(function (groupData) {
+          homeObject.group = groupData
+        });
+        //find all bill rows associated with the group
+        db.Bill.findAll({
+          where: {
+            GroupId: req.user.GroupId
+          }
+        }).then(function (billData) {
+          homeObject.bill = billData
+        });
+        db.Grocery.findAll({
+          where: {
+            GroupId: req.user.GroupId
+          }
+        }).then(function (groceryData) {
+          homeObject.grocery = groceryData;
+        });
+        db.Chore.findAll({
+          where: {
+            GroupId: req.user.GroupId
+          }
+        }).then(function (choreData) {
+          homeObject.chore = choreData;
+          console.log(homeObject);
+          //render with the home layout passing the homeObject 
+          res.render("home", homeObject);
+        });
+      }
+      //otherwise, if the user doesn't have a GroupId send them to the page where they can request to join/create groups
+      else {
+        res.redirect("/groupJoin")
+      }
+    });
 
-      })
-      db.Grocery.findAll({
-        where: {
-          GroupId: req.user.GroupId
-        }
-      }).then(function(groceryData){
-        homeObject.grocery = groceryData;
-       
-      })    
-      db.Chore.findAll({
-        where: {
-          GroupId: req.user.GroupId
-        }
-      }).then(function(choreData){
-        homeObject.chore = choreData;
-        console.log(homeObject);
-        //render with the home layout passing the homeObject 
-        res.render("home", homeObject);
-      })    
-
-    }
-    
-    //otherwise send them to the page where they can request to join/create groups
-    else{
-      res.redirect("/groupJoin")
-    }
   })
   //route for page where users can join/create groups
-  app.get("/groupJoin", isAuthenticated, function(req, res){
+  app.get("/groupJoin", isAuthenticated, function (req, res) {
     res.render("groupJoin");
   });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+  app.get("/example/:id", function (req, res) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function (dbExample) {
       res.render("example", {
         example: dbExample
       });
     });
   });
 
-  app.get("/bill/:creatorId/:billId", function(req, res){
+  app.get("/bill/:creatorId/:billId", function (req, res) {
     //if the user is the creator of the bill
     var hbsObject = {
       bill: ""
     }
     var creatorId = parseInt(req.params.creatorId);
     var billId = parseInt(req.params.billId);
-    if(req.user.id === creatorId){
+    if (req.user.id === creatorId) {
       db.Bill.findOne({
         where: {
           id: billId
         }
-      }).then(function(billData){
+      }).then(function (billData) {
         hbsObject.bill = billData
         res.render("billEdit", hbsObject);
       });
     }
-    else{
+    else {
       res.redirect("/home");
     }
   })
 
   // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
+  app.get("*", function (req, res) {
     res.render("404");
   });
-    //bec adding, might need to remove
-  app.get("/tasks", function(req, res) {
+  //bec adding, might need to remove
+  app.get("/tasks", function (req, res) {
     res.render("tasks");
   })
 
