ecf0520664f218e199db7a77166c7ee7be7d8c06
violettebec <bechoerner@gmail.com>
2019-03-30 17:35:06 -0500

    adding chat to chat tab

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -26,6 +26,11 @@ module.exports = function (app) {
     res.render("login");
   });
 
+  //user chat route
+  app.get("/chat", function (req, res) {
+    res.render("chat");
+  })
+
 
   //Load home page for authenticated user
   app.get("/home", isAuthenticated, function (req, res) {
@@ -124,6 +129,8 @@ module.exports = function (app) {
   app.get("/groupJoin", isAuthenticated, function (req, res) {
     res.render("groupJoin");
   });
+
+
 
 
   //-----------BILL--------------------------------------------------------------
